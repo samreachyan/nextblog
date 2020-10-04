f5be8f69394ea0a2c99a264e9c1ad7a3faf108b7
samreachyan <samreachyan@gmail.com>
2020-10-05 01:09:38 +0700

    passport strategy

---
+++ server/middlewares/passport/strategies.js
@@ -0,0 +1,30 @@
+const { Strategy } = require('passport-strategy')
+
+// Strategy get options(email, password) needed to authenticate user
+// Strategy gets a callback function that will contain functionality to verify an user
+// Strategy has to have "authenticate" function
+// Strategy has access to "error" "fail" "success" functions
+class GrpahqlStrategy extends Strategy {
+    constructor (verify) {
+        super()
+
+        if (!verify) {
+            throw new Error('Graphql strategy requires a verify callback')
+        }
+
+        this.verify = verifiy
+        this.name = 'graphql'
+    }
+
+    authenticate(_, options) {
+        console.log('Calling authenticate in strategy!')
+
+        const done = () => {
+            console.log('Calling done in authenticate callback')
+        }
+
+        this.verify(options, done)
+    }
+}
+
+module.exports = GrpahqlStrategy
