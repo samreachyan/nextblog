9d75a2277a95e2142759c733292f4d6a7095afaa
samreachyan <samreachyan@gmail.com>
2020-10-11 14:06:35 +0700

    validate password

--- server/middlewares/passport/index.js
+++ server/middlewares/passport/index.js
@@ -1,9 +1,10 @@
 const passport = require('passport')
 const GraphqlStrategy = require('./strategies')
 const User = require('../../database/models/user')
+const user = require('../../database/models/user')
 
 exports.init = (passport) => {
-    passport.use('graphql', GraphqlStrategy((options, done) => {
+    passport.use('graphql', GraphqlStrategy((options, password, done) => {
         User.findOne({ email}, (error, user) => {
             if (error) return done(error)
             if (!user) return done(null, false)
@@ -11,6 +12,11 @@ exports.init = (passport) => {
         // 1. Find user in DB and if use exists verify password
         // 2. If use is verify call 'done'
 
-        return done(null, user)
+        user.validatePassword(password, (error, isMatching) => {
+            if (error) return done(error)
+            if (!isMatching) return done(null, false)
+
+            return done(null, user)
+        })
     }))
 }
